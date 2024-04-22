85408afb8e2a0ed16861fb9096b6f6f592fb4c05
html.gyo <114139025+htmlgyo@users.noreply.github.com>
2024-04-22 23:42:22 +0800

    index.js

---
+++ cfbt_backend/index.js
@@ -0,0 +1,17 @@
+
+const express = require('express');
+const cors = require('cors'); // Import the cors package
+const productRoutes = require('./routes/toolRoutes');
+
+const app = express();
+
+// Middleware
+app.use(express.json());
+app.use(cors()); // Enable CORS for all routes
+
+// Routes
+app.use('/api/tools', productRoutes);
+
+// Start the server
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
