212f9e3d4192fcb064dae2bcf04d5ed494968863
unknown <geoffriebalahadia23@gmail.com>
2024-04-28 23:40:40 +0800

    login

---
+++ cfbt_frontend/register.js
@@ -0,0 +1,51 @@
+
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.11.1/firebase-app.js";
+import { getAuth, createUserWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/10.11.1/firebase-auth.js";
+
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyC8atiiA6w_oxp6bXvtGyvS9YEyNDynd5k",
+  authDomain: "consumerbusinessfinancetool.firebaseapp.com",
+  projectId: "consumerbusinessfinancetool",
+  storageBucket: "consumerbusinessfinancetool.appspot.com",
+  messagingSenderId: "597030601042",
+  appId: "1:597030601042:web:080c023e8fd493edfbfa8b"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+
+
+// submit Button
+const submit = document.getElementById('submit');
+submit.addEventListener("click", function (event) {
+  event.preventDefault()
+
+  //inputs
+  const email = document.getElementById('email').value;
+  const password = document.getElementById('password').value;
+
+  createUserWithEmailAndPassword(auth, email, password)
+    .then((userCredential) => {
+      //sign up
+      const user = userCredential.user;
+      alert("Account Created")
+      
+    })
+    .catch((error) => {
+      const errorCode = error.code;
+      const errorMessage = error.message;
+      alert(errorMessage)
+    });
+})
+
+
+// function for checking username and password
+
+
---
+++ cfbt_frontend/tools.js
@@ -0,0 +1,29 @@
+let products = null;
+        // get datas from file json 
+        fetch('http://localhost:3000/api/tools')
+            .then(response => response.json())
+            .then(data => {
+                products = data;
+                addDataToHTML();
+        })
+
+        function addDataToHTML(){
+     // remove datas default from HTML
+        let listProductHTML = document.querySelector('.listProduct');
+
+        // add new datas
+        if(products != null) // if has data
+        {
+            products.forEach(product => {
+                let newProduct = document.createElement('a');
+                newProduct.href = 'detail.html?id=' + product.id;
+                newProduct.classList.add('item');
+                newProduct.innerHTML = 
+                `<img src="images/${product.image}" alt="">
+                <p id="name">${product.name}</p>`;
+                listProductHTML.appendChild(newProduct);
+
+            });
+            
+        }
+     }
