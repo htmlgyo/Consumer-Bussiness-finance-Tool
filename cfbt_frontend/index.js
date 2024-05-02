0a582181f9806172bb2808d2d91a77e9355bbbed
unknown <geoffriebalahadia23@gmail.com>
2024-05-02 14:47:23 +0800

    geo

--- cfbt_frontend/index.js
+++ cfbt_frontend/index.js
@@ -1,6 +1,6 @@
 
 let products = null;
-// get datas from file json
+
 fetch('http://localhost:3000/api/tools')
     .then(response => response.json())
     .then(data => {
@@ -9,13 +9,13 @@ fetch('http://localhost:3000/api/tools')
     })
 
 function showDetail() {
-    // remove datas default from HTML
+  
     let detail = document.querySelector('.detail');
     let descriptionDetail = document.querySelector('.description-detail');
     let listProduct = document.querySelector('.listProduct');
     let productId = new URLSearchParams(window.location.search).get('id');
     let thisProduct = products.filter(value => value.id == productId)[0];
-    //if there is no product with id = productId => return to home page
+   
     if (!thisProduct) {
         window.location.href = "/";
     }
@@ -24,15 +24,14 @@ function showDetail() {
     detail.querySelector('.name').innerText = thisProduct.name;
     detail.querySelector('.tag').innerText = thisProduct.tags;
     detail.querySelector('.link').href = thisProduct.link;
-    descriptionDetail.querySelector('.distinct').innerText = thisProduct.distinct;
+    descriptionDetail.querySelector('.distinct').innerText = thisProduct.about;
 
     (products.filter(value => value.id != productId)).forEach(product => {
         let newProduct = document.createElement('a');
         newProduct.href = 'detail.html?id=' + product.id;
         newProduct.classList.add('item');
         newProduct.innerHTML =
-            `<img src="images/${product.image}" alt="">
-            <p>${product.name}</p>`;
+            `<img src="images/${product.image}" alt="">`;
         listProduct.appendChild(newProduct);
     });
 }
